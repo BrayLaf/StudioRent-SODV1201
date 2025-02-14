10b3b5dcae59f239d40106804cc0d6c5758ecc29
LorenceNoel <167948703+LorenceNoel@users.noreply.github.com>
2025-02-14 00:59:10 -0700

    Edited navbar and manage studio
    Edited the navbar and manage studio page

--- Manage-studios/Manage.js
+++ Manage-studios/Manage.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggle-Button');
+    const sideBar = document.getElementById('sideBar');
+
+    toggleButton.addEventListener('click', () => {
+        sideBar.classList.toggle('close');
+        toggleButton.classList.toggle('rotate');
+    });
+});

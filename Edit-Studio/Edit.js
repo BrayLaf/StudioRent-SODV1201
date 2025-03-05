b6ec0b28b88d8567fa1461868a95bea2cfa19b1f
BLafleur <BDLAFLEUR21@gmail.com>
2025-03-05 00:06:43 -0700

    fixed styling
    --footer needs fixing
    --issue with highlight

--- Edit-Studio/Edit.js
+++ Edit-Studio/Edit.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+$(function () {
     const mobileMenu = document.getElementById("mobile-menu");
     const navLinks = document.querySelector(".nav-links");
     const dropdown = document.querySelector(".dropdown");
@@ -21,4 +21,50 @@ document.addEventListener("DOMContentLoaded", function () {
             dropdown.classList.remove("open");
         }
     });
+
+    //Highlight what section you are on 
+    function isInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    
+        );
+    }
+    
+    
+    const keyinfo = document.querySelector(".btnDiv");
+    const images = document.querySelector(".imageUpload");
+    const location = document.querySelector(".propertyTitle");
+    const contact = document.querySelector(".contactDetails");
+    const sidebarelement = $(".sideKey1");
+    const sidebarelement2 = $(".sideKey2");
+    const sidebarelement3 = $(".sideKey3");
+    const sidebarelement4 = $(".sideKey4");
+
+    
+    document.addEventListener('scroll', function () {
+        sidebarelement.css({'background-color': 'var(--extraLight)', 'color': 'var(--text-dark)'});
+        sidebarelement2.css({'background-color': 'var(--extraLight)', 'color': 'var(--text-dark)'});
+        sidebarelement3.css({'background-color': 'var(--extraLight)', 'color': 'var(--text-dark)'});
+        sidebarelement4.css({'background-color': 'var(--extraLight)', 'color': 'var(--text-dark)'});
+
+        if(isInViewport(keyinfo))
+            sidebarelement.css({'background-color': 'var(--primaryColor3)', 'color': '#ffffff'})
+        
+        if(isInViewport(images))
+            sidebarelement2.css({'background-color': 'var(--primaryColor3)', 'color': '#ffffff'})
+        
+        if(isInViewport(location))
+            sidebarelement3.css({'background-color': 'var(--primaryColor3)', 'color': '#ffffff'});
+        
+        if(isInViewport(contact))
+            sidebarelement4.css({'background-color': 'var(--primaryColor3)', 'color': '#ffffff'});
+    }, {
+        passive: true
+    });
 });
+
+
